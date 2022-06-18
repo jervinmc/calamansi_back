24dfdad16029f3f529bf3eea8afa7998d98cb33c
jmacalawa <jmacalawa@futurex.ph>
2022-06-18 23:16:07 +0800

    dpone

--- app.py
+++ app.py
@@ -238,4 +238,4 @@ api.add_resource(Logs,'/api/v1/logs')
 api.add_resource(ResetPassword,'/api/v1/reset_password')
 # api.add_resource(UploadTest,'/api/v1/uploadtest')
 if __name__ == "__main__":
-    app.run(debug=True,host='localhost',port="5001")
+    app.run(debug=True,host='0.0.0.0',port="5000")
